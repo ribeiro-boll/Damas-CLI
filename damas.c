e083eb8b2b0a82e7980d829fdd73ad333c95982f
bolota <luccajetx@gmail.com>
2025-06-08 01:28:07 -0300

    inicio da criação de funções para mover as peças

--- damas.c
+++ damas.c
@@ -11,10 +11,8 @@
 #define PRETO 'p'
 #define BRANCO 'b'
 
-
 #define jogador_branco 0
 #define jogador_preto 1
-
 
 #define morto 0
 #define vivo 1
@@ -28,7 +26,10 @@ typedef struct{
 int contador_pecas_b = 0;
 int contador_pecas_p = 0;
 
+int jogador_atual = jogador_branco;
+
 char grid[8][8];
+
 void resetar_grid(char arr[8][8],Pecas_jogador **branco,Pecas_jogador **preto) {
     contador_pecas_b = 0;
     contador_pecas_p = 0;
@@ -68,12 +69,18 @@ void printar_structs_branco_e_preto(Pecas_jogador *branco,Pecas_jogador *preto){
     printf("\n");
     printf("\n");
     for(int i =0; i<contador_pecas_b;i++){
+        if (i%4==0 && i !=0){
+            printf(" |\n");
+        }
         printf(" | (%d,%d) - (%c,%d)",branco[i].coordenada_x,branco[i].coordenada_y,branco[i].coordenada_x+'A',branco[i].coordenada_y+1);
     }
     printf(" |\n");
     printf("\n");
     printf("\n");
     for(int i =0; i<contador_pecas_p;i++){
+        if (i%4==0 && i!=0){
+            printf(" |\n");
+        }
         printf(" | (%d,%d) - (%c,%d)",preto[i].coordenada_x,preto[i].coordenada_y,preto[i].coordenada_x+'A',preto[i].coordenada_y+1);
     }
     printf(" |\n\n");
@@ -93,71 +100,80 @@ void print_grid(char arr[8][8]){
     return;
 }
 
-
-void selecionar_peca(int arr[2]){
+void selecionar_peca(int arr[2],char grid[8][8],int jogador){
     char letra;
+    char *jogadores_str[2] = {"Jogador Branco","Jogador Preto"};
     int coordenada_y=-1;
     int coordenada_x=-1;
     int characteres_digitados = 0;
     printf("\n");
     while (1) {
         characteres_digitados = 0;
-        printf("Digite a coordenada da peça (coluna linha): ");
+        printf("Jogador atual: %s\nDigite a coordenada da peça (coluna linha): ",jogadores_str[jogador]);
         while ((letra = getchar())!='\n') {
             characteres_digitados++;
             if (characteres_digitados==1){
-                coordenada_y=toupper(letra)-'A';
+                coordenada_x=toupper(letra)-'A';
             }
             if (characteres_digitados==2){
-                coordenada_x=letra-'1';
+                coordenada_y=letra-'1';
             }
         }
-        printf("\n\nX: '%c'\nY: '%d'       \ngrid: '%c'\ncharacteres inputados '%d'\n", coordenada_x+'A', coordenada_y,grid[coordenada_x][coordenada_y], characteres_digitados);
+        printf("\n\nX: '%c' -> %d\nY: '%d' -> %d      \ngrid: '%c'\ncharacteres inputados '%d'\nJogador: %d", coordenada_x+'A',coordenada_x, coordenada_y+1,coordenada_y,grid[coordenada_y][coordenada_x], characteres_digitados,jogador);
         if (characteres_digitados > 2){
+            printf("\nDigite apenas duas coordenadas!\n");
             continue;
         }
         else if (coordenada_y >= 8 || coordenada_y < 0) {
+            printf("\nDigite uma coordenada dentro dos limites!\n");
             continue;
         }
         else if (coordenada_x >= 8 || coordenada_x < 0) {
+            printf("\nDigite uma coordenada dentro dos limites!\n");
             continue;
         }
+        else if (coordenada_x >= 8 || coordenada_x < 0) {
+            printf("\nDigite uma coordenada dentro dos limites!\n");
+            continue;
+        }
+        else if ((grid[coordenada_y][coordenada_x] == 'b' || grid[coordenada_y][coordenada_x] == 'B') && jogador == jogador_branco){
+            arr[0] = coordenada_y; // coluna
+            arr[1] = coordenada_x; // linha
+            printf("\n\n(%d,%d) -> %c",arr[0],arr[1],grid[coordenada_y][coordenada_x]);
+            break;
+        }
+        else if ((grid[coordenada_y][coordenada_x] == 'p' || grid[coordenada_y][coordenada_x] == 'P') && jogador == jogador_preto) {
+            arr[0] = coordenada_y; // coluna
+            arr[1] = coordenada_x; // linha
+            printf("\n\n(%d,%d) -> %c",coordenada_x,coordenada_y,grid[coordenada_y][coordenada_x]);
+            break;
+        }
         else {
-            break;\
+            printf("\nDigite a coordenada de uma peca correspondente a sua cor!\n");
+            continue;
         }
     }
-    
-    arr[0] = coordenada_y; // coluna
-    arr[1] = coordenada_x; // linha
 }
 
 // entraria a coordenada que fica no array "peca_selecionada"
-void mover_peca(int arr[2]){
+void mover_peca(int coordenada_peca[2],int jogador){
     
 }
 
-
 int main(){
-    
-Pecas_jogador *jogador_b = NULL;
-Pecas_jogador *jogador_p = NULL;
-
-    int jogador_atual = jogador_preto;
+    Pecas_jogador *jogador_b = NULL;
+    Pecas_jogador *jogador_p = NULL;
     int peca_selecionada[2];
     resetar_grid(grid,&jogador_b,&jogador_p);
     int ganhou_jogo = 0; 
     while(!ganhou_jogo){
         print_grid(grid);
         printar_structs_branco_e_preto(jogador_b, jogador_p);
-
-        /*
-        
-        selecionar_peca(peca_selecionada);
-        
-        
-        */
+        selecionar_peca(peca_selecionada,grid,jogador_atual);
         getchar();
         system("clear");
+        jogador_atual++;
+        jogador_atual%=2;
     }
     return 0;
 }
